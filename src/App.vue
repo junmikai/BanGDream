b2c4f133f472ff320147e8ecd1a09df993277c75
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-08-11 23:40:33 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <div class="header-container">
+      <img alt="logo" class="logo" src="../src/assets/images/logo.png" />
+      <div class="header-nav">
+        <div
+          v-for="list in englishs"
+          :key="list"
+          class="link"
+          :class="{ select: hoverLink === list }"
+          @mouseover="hoverLink = list"
+          @mouseout="hoverLink = ''"
+        >{{ list }}</div>
+
+        <img
+          v-for="icon in icons"
+          :key="icon"
+          :src="icon"
+          alt="sns"
+          class="sns"
+          :class="{ hoverOpacity: hoverSns === icon }"
+          @mouseover="hoverSns = icon"
+          @mouseout="hoverSns = ''"
+        />
+      </div>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  el: "#app",
+  data: function () {
+    return {
+      englishs: [
+        "NEWS",
+        "LIVE/EVENT",
+        "ANIME",
+        "MEDIA",
+        "DISCOGRAPHY",
+        "GOODS",
+        "ABOUT",
+        2222,
+      ],
+      icons: [
+        require("@/assets/images/twitter.png"),
+        require("@/assets/images/youtube.png"),
+        require("@/assets/images/instagram.png"),
+      ],
+      hoverLink: "",
+      hoverSns: "",
+    };
+  },
+  methods: {},
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="main-container">
+    <img
+      alt="main"
+      :key="main"
+      src="../../src/assets/images/Las.png"
+      :class="{ hoverOpacity: hoverMain === main }"
+      @mouseover="hoverMain = main"
+      @mouseout="hoverMain = ''"
+    />
+    <carousel
+      autoplay="true"
+      loop="true"
+      :per-page="1"
+      navigation-prev-label="〈"
+      navigation-next-label="〉"
+      :speed="1000"
+      :autoplayTimeout="4000"
+    >
+      <slide>
+        <img src="../../src/assets/images/banner1.png" alt="スライド1" />
+        <img src="../../src/assets/images/banner2.png" alt="スライド2" />
+      </slide>
+      <slide>
+        <img src="../../src/assets/images/banner2.png" alt="スライド2" />
+        <img src="../../src/assets/images/banner3.png" alt="スライド3" />
+      </slide>
+      <slide>
+        <img src="../../src/assets/images/banner3.png" alt="スライド3" />
+        <img src="../../src/assets/images/banner4.png" alt="スライド4" />
+      </slide>
+      <slide>
+        <img src="../../src/assets/images/banner4.png" alt="スライド4" />
+        <img src="../../src/assets/images/banner5.png" alt="スライド5" />
+      </slide>
+      <slide>
+        <img src="../../src/assets/images/banner5.png" alt="スライド5" />
+        <img src="../../src/assets/images/banner6.png" alt="スライド6" />
+      </slide>
+      <slide>
+        <img src="../../src/assets/images/banner6.png" alt="スライド6" />
+        <img src="../../src/assets/images/banner1.png" alt="スライド1" />
+      </slide>
+    </carousel>
+    <div class="news-container">
+      <p class="title">NEWS</p>
+      <p class="translation">ニュース</p>
+      <div class="lists">
+        <div class="news-list" @click="show = !show">
+          <div class="image">
+            <transition name="box">
+              <div class="target" v-if="show">
+                <img
+                  src="../../src/assets/images/target.png"
+                  alt="icon"
+                  class="icon"
+                />
+              </div>
+            </transition>
+            <img
+              src="../../src/assets/images/news1.png"
+              alt="thumbnail"
+              class="thumbnail"
+            />
+          </div>
+          <img
+            src="../../src/assets/images/info.png"
+            alt="genre"
+            class="genre"
+          />
+          <p class="title">【重要なお知らせ】2020年開催公演についてのご案内</p>
+          <p class="time">2020.06.30[Tue]</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data: function() {
+    return {
+      hoverMain: "",
+      show: false,
+    };
+  },
+};
+</script>
