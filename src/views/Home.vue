8aa4f8b307ee36ece2c4918602dfca92204641de
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-08-12 09:47:14 +0900

    ニュース全項目配列完了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -72,9 +72,9 @@
               class="thumbnail"
             />
           </div>
-          <img src="../../src/assets/images/info.png" alt="genre" class="genre" />
-          <p class="title">【重要なお知らせ】2020年開催公演についてのご案内</p>
-          <p class="time">2020.06.30[Tue]</p>
+          <img v-for="genre in list.genres" :key="genre" :src="genre" alt="genre" class="genre" />
+          <p class="title" v-for="title in list.titles" :key="title">{{title}}</p>
+          <p class="time" v-for="time in list.times" :key="time">{{time}}</p>
         </div>
       </div>
     </div>
@@ -89,14 +89,62 @@ export default {
       hoverThumbnail: "",
       escapeThumbnail: "",
       lists: [
-        { thumbnails: [require("@/assets/images/news1.png")] },
-        { thumbnails: [require("@/assets/images/news2.png")] },
-        { thumbnails: [require("@/assets/images/news3.png")] },
-        { thumbnails: [require("@/assets/images/news4.png")] },
-        { thumbnails: [require("@/assets/images/news5.png")] },
-        { thumbnails: [require("@/assets/images/news6.png")] },
-        { thumbnails: [require("@/assets/images/news6.png")] },
-        { thumbnails: [require("@/assets/images/news6.png")] },
+        {
+          thumbnails: [require("@/assets/images/news1.png")],
+          genres: [require("@/assets/images/info.png")],
+          titles: ["【重要なお知らせ】2020年開催公演についてのご案内"],
+          times: ["2020.06.30[Tue]"],
+        },
+        {
+          thumbnails: [require("@/assets/images/news2.png")],
+          genres: [require("@/assets/images/goods.png")],
+          titles: [
+            "BanG Dream! 8th☆LIVE「Breakthrough!」のグッズ情報第１弾を公開！",
+          ],
+          times: ["2020.08.06[Thu]"],
+        },
+        {
+          thumbnails: [require("@/assets/images/news3.png")],
+          genres: [require("@/assets/images/goods.png")],
+          titles: ["Morfonica 1st Live「Cantabile」のグッズ情報第１弾を公開！"],
+          times: ["2020.08.06[Thu]"],
+        },
+        {
+          thumbnails: [require("@/assets/images/news4.png")],
+          genres: [require("@/assets/images/info.png")],
+          titles: ["「バンドリ！TV LIVE 2020」第28回放送のお知らせ"],
+          times: ["2020.08.06[Thu]"],
+        },
+        {
+          thumbnails: [require("@/assets/images/news5.png")],
+          genres: [require("@/assets/images/info.png")],
+          titles: [
+            "「ハウスウェルネスフーズ×BanG Dream! 8th☆LIVEキャンペーン」開催！ ",
+          ],
+          times: ["2020.07.31[Fri]"],
+        },
+        {
+          thumbnails: [require("@/assets/images/no-image.png")],
+          genres: [require("@/assets/images/info.png")],
+          titles: [
+            "Afterglow×Pastel＊Palettes×ハロー、ハッピーワールド！6th Single 発売記念スペシャルトークイベント開催中止のお知らせ",
+          ],
+          times: ["2020.07.31[Fri]"],
+        },
+        {
+          thumbnails: [require("@/assets/images/no-image.png")],
+          genres: [require("@/assets/images/info.png")],
+          titles: [
+            "『Roselia 10th Single「約束」発売記念トークイベント』、『RAISE A SUILEN 4th Single「DRIVE US CRAZY」発売記念トーク&ミニライブ』開催中止のお知らせ",
+          ],
+          times: ["2020.07.31[Fri]"],
+        },
+        {
+          thumbnails: [require("@/assets/images/news6.png")],
+          genres: [require("@/assets/images/info.png")],
+          titles: ["ガルパと赤い羽根共同募金のコラボレーション第3弾が決定！"],
+          times: ["2020.07.30[Thu]"],
+        },
       ],
     };
   },
